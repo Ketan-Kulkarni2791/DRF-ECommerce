ddf06ea8fdb95e1e3fab3e73bbf25e296b3b7035
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-07-03 13:06:43 +0530

    Category viewset added

--- ECommerce/ECommerce/product/views.py
+++ ECommerce/ECommerce/product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryView(viewsets.ViewSet):
+    """A simple viewset for viewing categories."""
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
