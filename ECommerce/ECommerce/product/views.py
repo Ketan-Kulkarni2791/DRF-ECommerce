c58ad8f5f22f8496e1c528b7c99c35806030c70f
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-07-04 09:24:57 +0530

    swagger UI implemented

--- ECommerce/ECommerce/product/views.py
+++ ECommerce/ECommerce/product/views.py
@@ -1,15 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryView(viewsets.ViewSet):
+class CategoryViewSet(viewsets.ViewSet):
     """A simple viewset for viewing categories."""
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
